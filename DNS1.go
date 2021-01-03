2d8715aba3f79fc97f446bd2c290487a09300c3e
Gabriel <gabriel.pezoa@sansano.usm.cl>
2021-01-03 19:26:46 -0300

    DNS1 y protos

---
+++ DNS1.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net"
+	"fmt"
+	"google.golang.org/grpc"
+	"github.com/GabrielPR-usm/Tarea-3-SD/Mensajes/Consultas"
+	"github.com/GabrielPR-usm/Tarea-3-SD/Mensajes/Compartir"
+	"github.com/GabrielPR-usm/Tarea-3-SD/Mensajes/Modificaciones"
+
+)
+
+func consultasServer(lis net.Listener) {
+	s := Consulta.Server{}
+	grpcServer := grpc.NewServer()
+	Consulta.RegisterConsultaServiceServer(grpcServer, &s)
+	fmt.Println("DNS 1 Escuchando a las consultas de los clientes")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve consultas: %s", err)
+	}
+}
+
+func modificacionesServer(lis net.Listener) {
+	s := ModificarZF.Server{}
+	grpcServer := grpc.NewServer()
+	Consulta.RegisterModificarZFServiceServer(grpcServer, &s)
+	fmt.Println("DNS 1 Escuchando las modificaciones del admin")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve modificaciones: %s", err)
+	}
+}
+
+func compartirServer(lis net.Listener) {
+	s := CompartirZF.Server{}
+	grpcServer := grpc.NewServer()
+	Consulta.RegisterCompartirZFServiceServer(grpcServer, &s)
+	fmt.Println("DNS 1 Escuchando otros DNS para hacer consistencia")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve consistencia: %s", err)
+	}
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":1000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	go func(){
+		consultasServer(lis)
+	}()
+
+	go func(){
+		modificacionesServer(lis)
+	}()
+	
+	compartirServer(lis)
+}
